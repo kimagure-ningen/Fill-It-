7a766dc8ee150d796ecb1c9b02f5669b2a25a4be
Daito <daitoxyamagishi@gmail.com>
2023-12-11 23:28:30 +0900

    added ml agents

---
+++ gamefile/Assets/_Project/6_Sandbox/PlayerAgent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class PlayerAgent : Agent
+{
+    public int agentId;
+    
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+    }
+}
