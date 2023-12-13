8f07e78316a9f20a343a61efc7e8d0f7740a4199
Daito <daitoxyamagishi@gmail.com>
2023-12-13 21:35:22 +0900

    null

--- gamefile/Assets/_Project/6_Sandbox/PlayerAgent.cs
+++ gamefile/Assets/_Project/6_Sandbox/PlayerAgent.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
 public class PlayerAgent : Agent
 {
     public int agentId;
+    public GameObject enemyPlayer;
     
     public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(this.transform.localPosition);
+        sensor.AddObservation(enemyPlayer.transform.localPosition);
+    }
+
+    public void OnVictory()
+    {
+        AddReward(0.1f);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         
     }
